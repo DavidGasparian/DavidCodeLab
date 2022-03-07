e47eb017e7dcd5811f99edeca7f6dad544a85830
Zver <dav200609@gmao;.com>
2022-03-07 15:35:56 +0300

    '1'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Startup Nаme Generator',
       theme: ThemeData(          // Add the 5 lines from here...
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
